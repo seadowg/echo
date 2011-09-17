fee54945f28c964020af13109c1c9ec85403ba0c
Callum Stott <oetzi101@gmail.com>
2011-09-17 16:33:15 +0100

    Quick refactor

--- src/main/scala/com/github/oetzi/echo/Behaviour.scala
+++ src/main/scala/com/github/oetzi/echo/Behaviour.scala
@@ -3,7 +3,7 @@ import java.util.Date
 package com.github.oetzi.echo {
 	class Behaviour[T](rule : Double => T) {
 		def now() : T = {
-			rule((new Date()).getTime)
+			rule(new Date().getTime)
 		}
 	}
 }
