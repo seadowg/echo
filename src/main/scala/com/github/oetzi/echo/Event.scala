a5e730b62cabfac52624da29714fe126ad5101ee
Callum Stott <oetzi101@gmail.com>
2011-09-18 18:56:52 +0100

    Implemented (cheapo easy) Event

--- src/main/scala/com/github/oetzi/echo/Event.scala
+++ src/main/scala/com/github/oetzi/echo/Event.scala
@@ -1,5 +1,13 @@
 package com.github.oetzi.echo {
 	class Event[T] {
+		var edges : List[T => Any] = List[T => Any]()
 		
+		def occur(event : T) {
+			edges.foreach(edge => edge(event))
+		}
+		
+		def each(edge : T => Any) {
+			edges = edges ++ List[T => Any](edge)
+		}
 	}
 }
