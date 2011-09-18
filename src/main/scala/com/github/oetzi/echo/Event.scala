7ee525b78de75242a6a3b2b096bbdb330f1fc9cf
Callum Stott <oetzi101@gmail.com>
2011-09-18 16:52:43 +0100

    Add Event.scala

---
+++ src/main/scala/com/github/oetzi/echo/Event.scala
@@ -0,0 +1,5 @@
+package com.github.oetzi.echo {
+	class Event {
+		
+	}
+}
---
+++ src/spec/scala/EventSpec.scala
@@ -0,0 +1,8 @@
+import org.specs._
+import com.github.oetzi.echo.Event
+
+object EventSpec extends Specification {
+	"Event" should {
+		
+	}
+}
