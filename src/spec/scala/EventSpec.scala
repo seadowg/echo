e0e00eadecc57bf5d7a4f0b1a31c692b8ee96cfb
Callum Stott <oetzi101@gmail.com>
2011-09-20 16:45:44 +0100

    Add test to ensure edges are executed in order

--- src/spec/scala/EventSpec.scala
+++ src/spec/scala/EventSpec.scala
@@ -1,5 +1,6 @@
 import org.specs._
 import com.github.oetzi.echo.Event
+import java.util.Date
 
 object EventSpec extends Specification {
 	"Event" should {
@@ -18,13 +19,28 @@ object EventSpec extends Specification {
 	}
 	
 	"Event.occur function" should {
-		"execute assigned 'edges'" in {
+		"executes assigned 'edges'" in {
 			val event = new Event[Int]
 			var x = 5
 			
 			event.each(event => x = event)
 			event.occur(10)
 			x mustBe 10
+		}
+		
+		"executes edges in the corret order" in {
+			val event = new Event[Int]
+			var first : Double = 0
+			var second : Double = 0
+			
+			event.each({
+				event => first = new Date().getTime;
+				Thread.sleep(1);
+			})
+			event.each(event => second = new Date().getTime)
+			event.occur(5)
+			
+			first < second mustBe true
 		}
 	}
 }
