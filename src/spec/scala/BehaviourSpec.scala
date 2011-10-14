321e2b649ee59ff3d855a2818b9d5ca9a5fcb5b8
Callum Stott <oetzi101@gmail.com>
2011-10-14 19:15:36 +0100

    Add changes functio to Behaviour

--- src/spec/scala/BehaviourSpec.scala
+++ src/spec/scala/BehaviourSpec.scala
@@ -146,4 +146,27 @@ object BehaviourSpec extends Specification {
 			until_beh.now mustBe 10
 		}
 	}
+	
+	"'Behaviour.changes' function" should {
+		"return a Witness for the Behaviour" in {
+			val beh = new Behaviour(time => 5)
+			var fired = false
+			
+			beh.changes.each(event => fired = true)
+			beh.change(time => 1)
+			
+			val then = new Behaviour(time => time).now
+			val now = new Behaviour(time => time)
+			
+			while (!fired && now.now < then + 1000) {}
+			
+			fired mustBe true
+		}
+		
+		"return the same Witness instance for each call" in {
+			val beh = new Behaviour(time => 5)
+			
+			beh.changes mustBe beh.changes
+		}
+	}
 }
