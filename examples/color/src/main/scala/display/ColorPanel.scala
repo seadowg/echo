d74882ba539ee95bb8ebf670036636b8af6d7a33
Callum Stott <oetzi101@gmail.com>
2011-10-22 23:00:27 +0100

    Update examples

--- examples/color/src/main/scala/display/ColorPanel.scala
+++ examples/color/src/main/scala/display/ColorPanel.scala
@@ -1,13 +1,23 @@
 import java.awt.Color
 import javax.swing.JPanel
 import com.github.oetzi.echo.Behaviour
-import com.github.oetzi.echo.Witness
+import com.github.oetzi.echo.Event
 
 package display {
 	class ColorPanel extends JPanel {
 		def setBackground(color : Behaviour[Color]) {
-			super.setBackground(color.now)
-			color.sample.foreach(change => super.setBackground(change))	
+			val sampler = new Event[Unit]
+			
+			new Thread(new Runnable() {
+				def run {
+					while (true) {
+						sampler.occur()
+						Thread.sleep(40)
+					}
+				}
+			}).start
+			
+			color.sample(sampler).foreach(change => super.setBackground(change))	
 		}
 	}
 }
