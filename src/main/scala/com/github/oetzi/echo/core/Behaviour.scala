f62a82980be55e6c669fed6b46f4ff3c65275f05
Callum Stott <oetzi101@gmail.com>
2011-11-18 23:54:59 +0000

    Remove all my delirious ramblings

--- src/main/scala/com/github/oetzi/echo/core/Behaviour.scala
+++ src/main/scala/com/github/oetzi/echo/core/Behaviour.scala
@@ -1,17 +1,13 @@
 import com.github.oetzi.echo.Echo._
 
 package com.github.oetzi.echo.core {
-	abstract class Continuous[T] {
-		def now : T
-		
-		override def toString() : String = {
-			this.now.toString
-		}
-	}
-	
-	class Behaviour[T](val rule : Time => T) extends Continuous[T]() {
+	class Behaviour[T](val rule : Time => T) {
 		def now() : T = {
 			this.at(System.currentTimeMillis)
+		}
+		
+		def at(time : Time) : T = {
+			rule(time)
 		}
 		
 		def sample[A](event : EventSource[A]) : Event[T] = {
@@ -41,18 +37,8 @@ package com.github.oetzi.echo.core {
 			new Behaviour(time => func(this.at(time)))
 		}
 		
-		def at(time : Time) : T = {
-			rule(time)
+		override def toString() : String = {
+			this.now.toString
 		}
 	}
-	
-	class Reactive[T](val rule : () => T) extends Continuous[T]() {
-		def now() : T = {
-			rule()
-		}
-	}
-	
-	class EmbdContinuous[T](val self : Continuous[T]) { }
-	
-	case class NonDeterminismException() extends Exception { }
 }
