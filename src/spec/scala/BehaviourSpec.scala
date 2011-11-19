308ede612b60a8577b8c7befc85013fc81f109da
Callum Stott <oetzi101@gmail.com>
2011-11-19 00:16:21 +0000

    Refactor tests

--- src/spec/scala/BehaviourSpec.scala
+++ src/spec/scala/BehaviourSpec.scala
@@ -14,91 +14,91 @@ object BehaviourSpec extends Specification {
 			val beh = new Behaviour(time => 5)
 			beh.toString mustEqual "5"
 		}
-	}
-	
-	"'Behaviour.now' function" should {
-		"should return the result of rule()" in {
-			val beh = new Behaviour(time => (5 + 6))
-			beh.now mustBe 11
-		}
-	}
-	
-	"'Behaviour.at' function" should {
-		"should return the result of the rule with the passed value" in {
-			val beh = new Behaviour(time => time * 5)
-			beh.at(5).asInstanceOf[Int] mustBe 25
-		}
-	}
-	
-	"Behaviour.until" should {
-		"return a new Behaviour" in {
-			val beh = new Behaviour(time => 5)
-			
-			beh.until(new Event[Int], time => 5) must_!= beh
-		}
 		
-		"return a new Behaviour with the current rule when the Event hasn't occured" in {
-			val beh = new Behaviour(time => 5)
-			
-			beh.until(new Event[Int], time => 10).now mustBe 5
+		"provide a now function" >> {
+			"returning the result of rule()" in {
+				val beh = new Behaviour(time => (5 + 6))
+				beh.now mustBe 11
+			}
 		}
-		
-		"return a new Behaviour with the new rule after the Event occurs" in {
-			var beh = new Behaviour(time => 5)
-			val event = new Event[Int]
-			beh = beh.until(event, time => 10)
-			event.occur(new Occurence(System.currentTimeMillis, 5))
-			
-			beh.now mustBe 10
+
+		"provide an at function" >> {
+			"returning the result of the rule with the passed value" in {
+				val beh = new Behaviour(time => time * 5)
+				beh.at(5).asInstanceOf[Int] mustBe 25
+			}
 		}
-		
-		"return a Behaviour thats rule only 'changes' if the time is after the event" in {
-			val event = new Event[Int]
-			val beh = new Behaviour(time => 5).until(event, time => 10)
-			event.occur(new Occurence(System.currentTimeMillis, 5))
-			
-			beh.at(1) mustBe 5
+
+		"provide an until function" >> {
+			"returning a new Behaviour" in {
+				val beh = new Behaviour(time => 5)
+
+				beh.until(new Event[Int], time => 5) must_!= beh
+			}
+
+			"returning a new Behaviour with the current rule when the Event hasn't occured" in {
+				val beh = new Behaviour(time => 5)
+
+				beh.until(new Event[Int], time => 10).now mustBe 5
+			}
+
+			"returning a new Behaviour with the new rule after the Event occurs" in {
+				var beh = new Behaviour(time => 5)
+				val event = new Event[Int]
+				beh = beh.until(event, time => 10)
+				event.occur(new Occurence(System.currentTimeMillis, 5))
+
+				beh.now mustBe 10
+			}
+
+			"returning a Behaviour thats rule only 'changes' if the time is after the event" in {
+				val event = new Event[Int]
+				val beh = new Behaviour(time => 5).until(event, time => 10)
+				event.occur(new Occurence(System.currentTimeMillis, 5))
+
+				beh.at(1) mustBe 5
+			}
 		}
-	}
-	
-	"'Behaviour.sample' function" should {
-		"return a new Event when passed an Event of any type" in {
-			val beh = new Behaviour(time => 5)
-			val event = new Event[Unit]
-			
-			beh.sample(event).isInstanceOf[Event[Int]] mustBe true
+
+		"provide a sample function" >> {
+			"returning a new Event when passed an Event of any type" in {
+				val beh = new Behaviour(time => 5)
+				val event = new Event[Unit]
+
+				beh.sample(event).isInstanceOf[Event[Int]] mustBe true
+			}
+
+			"returning an Event that fires when the passed in event fires" in {
+				val beh = new Behaviour(time => 5)
+				val event = new Event[Int]
+
+				val sampler = beh.sample(event)
+				event.occur(new Occurence(10, 5))
+
+				sampler.occs.isEmpty mustBe false
+			}
+
+			"returning an Event that occurs with the current value of the Behaviour" in {
+				val beh = new Behaviour(time => 5)
+				val event = new Event[Int]
+				var firedVal = 0
+
+				val sampler = beh.sample(event)
+				val occ = new Occurence(10, 5)
+				event.occur(occ)
+
+				sampler.occs.last.time mustBe occ.time
+				sampler.occs.last.value mustBe occ.value
+			}
 		}
-		
-		"return an Event that fires when the passed in event fires" in {
-			val beh = new Behaviour(time => 5)
-			val event = new Event[Int]
-			
-			val sampler = beh.sample(event)
-			event.occur(new Occurence(10, 5))
-			
-			sampler.occs.isEmpty mustBe false
-		}
-		
-		"return an Event that occurs with the current value of the Behaviour" in {
-			val beh = new Behaviour(time => 5)
-			val event = new Event[Int]
-			var firedVal = 0
-			
-			val sampler = beh.sample(event)
-			val occ = new Occurence(10, 5)
-			event.occur(occ)
-			
-			sampler.occs.last.time mustBe occ.time
-			sampler.occs.last.value mustBe occ.value
-		}
-	}
-	
-	"'Behaviour.map' function" should {
-		"return a new Behaviour of type B (for map(func : T => B))" in {
-			val beh = new Behaviour(time => 5)
-			val func : Int => String = { int => int.toString }
-			
-			beh.map(func).isInstanceOf[Behaviour[String]] mustBe true
+
+		"provide a map function" >> {
+			"returning a new Behaviour of type B (for map(func : T => B))" in {
+				val beh = new Behaviour(time => 5)
+				val func : Int => String = { int => int.toString }
+
+				beh.map(func).isInstanceOf[Behaviour[String]] mustBe true
+			}
 		}
 	}
 }
--- src/spec/scala/EventSpec.scala
+++ src/spec/scala/EventSpec.scala
@@ -9,129 +9,129 @@ object EventSpec extends Specification {
 			val event = new Event[Int]
 			event.isInstanceOf[Event[Int]] mustBe true
 		}
-	}
-	
-	"Event.occs" should {
-		"return an empty list for new Events" in {
-			val event = new Event[Int]
-			
-			event.occs.isEmpty mustBe true
-		}
 		
-		"returns a List with the correct length" in {
-			val event = new Event[Int]
-			event.occur(new Occurence(now, 5))
-			event.occur(new Occurence(now, 5))
-			
-			event.occs.length mustBe 2
-		}
-		
-		"returns a list with the correct values" in {
-			val event = new Event[Int]
-			val occurence = new Occurence(now, 5)
-			event.occur(occurence)
-			
-			event.occs()(0) mustBe occurence
-		}
-	}
-	
-	"Event.occur" should {
-		"increase the length of occs" in {
-			val event = new Event[Int]
-			val length = event.occs.length
-			event.occur(new Occurence(now, 5))
-			
-			event.occs.length mustBe length + 1
-		}
-		
-		"maintains order on added occurences" in {
-			val event = new Event[Int]
-			event.occur(new Occurence(15, 5))
-			event.occur(new Occurence(10, 5))
-			
-			event.occs.last.time mustBe 15L
-		}
-	}
-	
-	"Event.foreach" should {
-		"should create a function that is executed on each future occurence" in {
-			val event = new Event[Int]
-			var fired = false
-			
-			event.foreach { occurence =>
-				fired = true
+		"provide an occs function" >> {
+			"returning an empty list for new Events" in {
+				val event = new Event[Int]
+
+				event.occs.isEmpty mustBe true
 			}
-			
-			event.occur(new Occurence(0, 10))
-			
-			fired mustBe true
-		}
-		
-		"should create a function that is executed on each past occurence" in {
-			val event = new Event[Int]
-			var fired = false
-			event.occur(new Occurence(0, 10))
-			
-			event.foreach { occurence =>
-				fired = true
+
+			"returning a List with the correct length" in {
+				val event = new Event[Int]
+				event.occur(new Occurence(now, 5))
+				event.occur(new Occurence(now, 5))
+
+				event.occs.length mustBe 2
 			}
-			
-			fired mustBe true
+
+			"returning a list with the correct values" in {
+				val event = new Event[Int]
+				val occurence = new Occurence(now, 5)
+				event.occur(occurence)
+
+				event.occs()(0) mustBe occurence
+			}
 		}
-	}
-	
-	"Event.merge" should {
-		"return an Event that contains all the occurences of each" in {
-			val eventOne = new Event[Int]
-			val eventTwo = new Event[Int]
-			val occurOne = new Occurence(now, 5)
-			val occurTwo = new Occurence(now, 5)
-			eventOne.occur(occurOne)
-			eventTwo.occur(occurTwo)
-			var matcher = List(occurOne, occurTwo)
-			
-			eventOne.merge(eventTwo).occs.foreach { occurence => 
-				if (matcher.contains(occurence)) {
-					matcher = matcher -- List(occurence)
+
+		"provide an occur function" >> {
+			"that increase the length of occs" in {
+				val event = new Event[Int]
+				val length = event.occs.length
+				event.occur(new Occurence(now, 5))
+
+				event.occs.length mustBe length + 1
+			}
+
+			"that maintains order on added occurences" in {
+				val event = new Event[Int]
+				event.occur(new Occurence(15, 5))
+				event.occur(new Occurence(10, 5))
+
+				event.occs.last.time mustBe 15L
+			}
+		}
+
+		"provide a foreach function" >> {
+			"that creates a function that is executed on each future occurence" in {
+				val event = new Event[Int]
+				var fired = false
+
+				event.foreach { occurence =>
+					fired = true
+				}
+
+				event.occur(new Occurence(0, 10))
+
+				fired mustBe true
+			}
+
+			"that creates a function that is executed on each past occurence" in {
+				val event = new Event[Int]
+				var fired = false
+				event.occur(new Occurence(0, 10))
+
+				event.foreach { occurence =>
+					fired = true
+				}
+
+				fired mustBe true
+			}
+		}
+
+		"provide a merge function" >> {
+			"returning an Event that contains all the occurences of each" in {
+				val eventOne = new Event[Int]
+				val eventTwo = new Event[Int]
+				val occurOne = new Occurence(now, 5)
+				val occurTwo = new Occurence(now, 5)
+				eventOne.occur(occurOne)
+				eventTwo.occur(occurTwo)
+				var matcher = List(occurOne, occurTwo)
+
+				eventOne.merge(eventTwo).occs.foreach { occurence => 
+					if (matcher.contains(occurence)) {
+						matcher = matcher -- List(occurence)
+					}
+				}
+
+				matcher.isEmpty mustBe true
+			}
+
+			"returning an Event with ordered occurences of each" in {
+				val eventOne = new Event[Int]
+				val eventTwo = new Event[Int]
+				val occurOne = new Occurence(10, 5)
+				val occurTwo = new Occurence(15, 5)
+				eventOne.occur(occurOne)
+				eventTwo.occur(occurTwo)
+				var last : Occurence[Int] = new Occurence(0, 0)
+
+				eventTwo.merge(eventOne).occs.foreach { occurence => 
+					last.time must be_<=(occurence.time)
+					last = occurence	
 				}
 			}
-			
-			matcher.isEmpty mustBe true
-		}
-		
-		"return an Event with ordered occurences of each" in {
-			val eventOne = new Event[Int]
-			val eventTwo = new Event[Int]
-			val occurOne = new Occurence(10, 5)
-			val occurTwo = new Occurence(15, 5)
-			eventOne.occur(occurOne)
-			eventTwo.occur(occurTwo)
-			var last : Occurence[Int] = new Occurence(0, 0)
-			
-			eventTwo.merge(eventOne).occs.foreach { occurence => 
-				last.time must be_<=(occurence.time)
-				last = occurence	
+
+			"returning an Event that updates when the original updates" in {
+				val eventOne = new Event[Int]
+				val eventTwo = new Event[Int]
+
+				val combEvent = eventOne.merge(eventTwo)
+				eventOne.occur(new Occurence(now, 5))
+
+				combEvent.occs.length mustBe 1
 			}
-		}
-		
-		"return an Event that updates when the original updates" in {
-			val eventOne = new Event[Int]
-			val eventTwo = new Event[Int]
-			
-			val combEvent = eventOne.merge(eventTwo)
-			eventOne.occur(new Occurence(now, 5))
-			
-			combEvent.occs.length mustBe 1
-		}
-		
-		"return an Event that updates when the paramater updates" in {
-			val eventOne = new Event[Int]
-			val eventTwo = new Event[Int]
-			
-			val combEvent = eventOne.merge(eventTwo)
-			eventTwo.occur(new Occurence(now, 5))
-			
-			combEvent.occs.length mustBe 1
+
+			"returning an Event that updates when the paramater updates" in {
+				val eventOne = new Event[Int]
+				val eventTwo = new Event[Int]
+
+				val combEvent = eventOne.merge(eventTwo)
+				eventTwo.occur(new Occurence(now, 5))
+
+				combEvent.occs.length mustBe 1
+			}
 		}
 	}
 }
