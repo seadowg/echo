cda6d13581623ab99dd4bb69c5622c5b0a079b2a
Callum Stott <oetzi101@gmail.com>
2012-01-16 00:09:09 +0000

    Make Sender event sending asynchronous

--- src/main/scala/com/github/oetzi/echo/io/Sender.scala
+++ src/main/scala/com/github/oetzi/echo/io/Sender.scala
@@ -3,6 +3,7 @@ package com.github.oetzi.echo.io
 import com.github.oetzi.echo.Echo._
 import com.github.oetzi.echo.core.{Occurrence, Event, EventSource}
 import java.io.PrintWriter
+import actors.Actor
 
 class Sender(val ip: String, val port: Int, event: EventSource[String]) {
   val errors: Event[Exception] = Event[Exception]
@@ -10,11 +11,7 @@ class Sender(val ip: String, val port: Int, event: EventSource[String]) {
   event.map {
     occ =>
       try {
-        val socket = new java.net.Socket(ip, port)
-        val out = new PrintWriter(socket.getOutputStream(), true)
-        out.println(occ.value)
-        out.close()
-        socket.close()
+        SenderActor ! occ.value
       }
 
       catch {
@@ -22,4 +19,23 @@ class Sender(val ip: String, val port: Int, event: EventSource[String]) {
       }
       occ
   }
+
+  private object SenderActor extends Actor {
+    def act = {
+      loop {
+        react {
+          case message: String => sendToSocket(message)
+        }
+      }
+    }
+
+    def sendToSocket(message: String) {
+      val socket = new java.net.Socket(ip, port)
+      val out = new PrintWriter(socket.getOutputStream(), true)
+      out.println(message)
+      out.close()
+      socket.close()
+    }
+  }
+
 }
