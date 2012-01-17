652bcab450c71e294b8b34df7890d9f6be3fa76a
Callum Stott <oetzi101@gmail.com>
2012-01-17 23:04:32 +0000

    Use 'dangerous' function for errors in Breakable

--- src/main/scala/com/github/oetzi/echo/io/Receiver.scala
+++ src/main/scala/com/github/oetzi/echo/io/Receiver.scala
@@ -2,7 +2,7 @@ package com.github.oetzi.echo.io
 
 import java.lang.Thread
 import java.net.ServerSocket
-import com.github.oetzi.echo.core.{Occurrence, Event, EventSource}
+import com.github.oetzi.echo.core.{Occurrence, EventSource}
 import com.github.oetzi.echo.Echo._
 import java.io.{InputStreamReader, BufferedReader}
 
@@ -10,20 +10,17 @@ import java.io.{InputStreamReader, BufferedReader}
 class Receiver(val port: Int) extends EventSource[String] with Breakable {
   new Thread(new Runnable() {
     def run() {
-      try {
-        val socket = new ServerSocket(port)
+      dangerous {
+        () =>
+          val socket = new ServerSocket(port)
 
-        while (true) {
-          val request = socket.accept()
-          val in = new BufferedReader(new InputStreamReader(request.getInputStream()))
-          Receiver.this.occur(new Occurrence(now, in.readLine()))
-          in.close()
-          request.close()
-        }
-      }
-
-      catch {
-        case e: Exception => errors.occur(new Occurrence(now, e))
+          while (true) {
+            val request = socket.accept()
+            val in = new BufferedReader(new InputStreamReader(request.getInputStream()))
+            Receiver.this.occur(new Occurrence(now, in.readLine()))
+            in.close()
+            request.close()
+          }
       }
     }
   }).start()
--- src/main/scala/com/github/oetzi/echo/io/Sender.scala
+++ src/main/scala/com/github/oetzi/echo/io/Sender.scala
@@ -1,25 +1,18 @@
 package com.github.oetzi.echo.io
 
-import com.github.oetzi.echo.Echo._
-import com.github.oetzi.echo.core.{Occurrence, Event, EventSource}
+import com.github.oetzi.echo.core.EventSource
 import java.io.PrintWriter
 import actors.Actor
 
 class Sender(val ip: String, val port: Int, event: EventSource[String]) extends Breakable {
   event.map {
     occ =>
-      try {
-        SenderActor ! occ.value
-      }
-
-      catch {
-        case e: Exception => this.errors.occur(new Occurrence(now, e))
-      }
+      SenderActor ! occ.value
       occ
   }
 
   private object SenderActor extends Actor {
-    def act = {
+    def act {
       loop {
         react {
           case message: String => sendToSocket(message)
@@ -28,11 +21,15 @@ class Sender(val ip: String, val port: Int, event: EventSource[String]) extends 
     }
 
     def sendToSocket(message: String) {
-      val socket = new java.net.Socket(ip, port)
-      val out = new PrintWriter(socket.getOutputStream(), true)
-      out.println(message)
-      out.close()
-      socket.close()
+      dangerous {
+        () =>
+          val socket = new java.net.Socket(ip, port)
+          val out = new PrintWriter(socket.getOutputStream(), true)
+          out.println(message)
+          out.close()
+          socket.close()
+      }
     }
   }
+
 }
