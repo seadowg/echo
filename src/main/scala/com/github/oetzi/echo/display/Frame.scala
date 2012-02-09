f6073bd07a8806031abc7dced92728681439caac
Callum Stott <oetzi101@gmail.com>
2012-02-09 16:18:41 +0000

    Fix setVisible layout breaking and addnew Text component

--- src/main/scala/com/github/oetzi/echo/display/Frame.scala
+++ src/main/scala/com/github/oetzi/echo/display/Frame.scala
@@ -8,7 +8,7 @@ import java.awt.Point
 import com.github.oetzi.echo.core.{Event, Occurrence, Behaviour}
 import com.github.oetzi.echo.types.Stepper
 
-class Frame private(val visibleBeh: Behaviour[Boolean]) extends Canvas {
+class Frame private(private val visibleBeh: Behaviour[Boolean]) extends Canvas {
   private var components: List[Canvas] = List[Canvas]()
 
   val internal: JFrame = new JFrame() {
@@ -63,10 +63,17 @@ class Frame private(val visibleBeh: Behaviour[Boolean]) extends Canvas {
         canvas.update(occurrence)
     }
   }
+  
+  private var lastVis = false
 
   def draw(occurrence: Occurrence[Unit]) {
     this.internal.setSize(widthBeh.at(occurrence.time), heightBeh.at(occurrence.time))
-    this.internal.setVisible(visibleBeh.at(occurrence.time))
+    
+    val vis = visibleBeh.at(occurrence.time)
+    if (vis != lastVis) {
+      this.internal.setVisible(vis)
+      lastVis = vis
+    }
 
     this.internal.repaint()
 
---
+++ src/main/scala/com/github/oetzi/echo/display/Text.scala
@@ -0,0 +1,31 @@
+package com.github.oetzi.echo.display
+
+import javax.swing.JLabel
+import com.github.oetzi.echo.core.{Behaviour, Occurrence}
+import com.github.oetzi.echo.Echo._
+
+class Text private(private val textBeh : Behaviour[String]) extends Canvas {
+  val internal : JLabel = new JLabel() {
+    override def repaint() {
+      Text.this.update(new Occurrence(now, ()))
+      super.repaint()
+    }
+  }
+
+  def update(occurrence: Occurrence[Unit]) {
+    redraw.occur(occurrence)
+  }
+
+  def draw(occurrence: Occurrence[Unit]) {
+    this.internal.setSize(widthBeh.at(occurrence.time), heightBeh.at(occurrence.time))
+    this.internal.setText(textBeh.at(occurrence.time))
+
+    this.internal.repaint()
+  }
+}
+
+object Text {
+  def apply(text : Behaviour[String]) : Text = {
+    new Text(text)
+  }
+}
