427e1e0bdcc36892f9a17a30eb36649b59237978
Callum Stott <oetzi101@gmail.com>
2012-03-09 23:03:51 +0000

    Freeze time for EchoApp.setup

--- src/main/scala/com/github/oetzi/echo/EchoApp.scala
+++ src/main/scala/com/github/oetzi/echo/EchoApp.scala
@@ -1,13 +1,18 @@
 package com.github.oetzi.echo
 
 import com.github.oetzi.echo.Control._
+import com.github.oetzi.echo.Echo._
 
 abstract class EchoApp {
 	def setup(args : Array[String])
 	
 	def main(args : Array[String]) {
 		writeLock.acquire()
-		setup(args)
+		
+		freezeTime(now()) {
+			() => setup(args)
+		}
+		
 		writeLock.release()
 	}
 }
