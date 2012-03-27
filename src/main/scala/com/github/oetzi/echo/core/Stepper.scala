79469ca5feda747c5d8cc508409df2b57797bdfc
Callum Stott <oetzi101@gmail.com>
2012-03-27 21:04:13 +0100

    Redefine Stepper in terms of Switcher

--- src/main/scala/com/github/oetzi/echo/core/Stepper.scala
+++ src/main/scala/com/github/oetzi/echo/core/Stepper.scala
@@ -3,27 +3,10 @@ package com.github.oetzi.echo.core
 import com.github.oetzi.echo.Echo._
 import com.github.oetzi.echo.Control._
 
-class Stepper[T](initial: T, val event: Event[T]) extends Behavior[T](Stepper.construct(initial, event)) {}
+class Stepper[T](initial: T, event: Event[T]) extends Switcher[T](initial, event.map((t, v) => new Constant(v))) {}
 
 object Stepper {
   def apply[T](initial: T, event: Event[T]) : Stepper[T] = {
     new Stepper(initial, event)
-  }
-  
-  private def construct[T](initial: T, event: Event[T]): Time => T = {
-    frp {
-      () => {
-        time =>
-          val occ = event.top(time)
-
-          if (occ == None) {
-            initial
-          }
-
-          else {
-            occ.get.value
-          }
-      }
-    }
   }
 }
