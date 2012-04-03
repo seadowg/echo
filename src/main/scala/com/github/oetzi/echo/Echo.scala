5999ea42de545ed60e3b60472feff80646c5aba4
Callum Stott <oetzi101@gmail.com>
2012-04-03 12:24:48 +0100

    Comment Echo.scala

--- src/main/scala/com/github/oetzi/echo/Echo.scala
+++ src/main/scala/com/github/oetzi/echo/Echo.scala
@@ -9,6 +9,10 @@ object Echo {
   private var fakeTime: Time = 0
   private var startTime: Time = 0
 
+  /** Retuns the current Echo time. This
+    * time is the time since the program started
+    * running in nano seconds.
+   */
   def now(): Time = {
     if (this.fake > 0) {
       this.fakeTime
@@ -19,14 +23,33 @@ object Echo {
     }
   }
 
+  /** Implicit function for lifiting static values
+    * to Behaviours.
+   */
   implicit def lift[T](value: T): Behaviour[T] = new Constant(value)
 
+  /** Type alias for Time.
+    */
   type Time = Double
 
+  /** Starts the Echo clock at the beginning
+    * of program execution.
+   */
   private[echo] def startClock() {
     startTime = System.nanoTime()
   }
 
+  /** Allows Echo's notion of time to be frozen
+    * at the specified time for the given block's execution.
+   */
+  private[echo] def freezeTime[T](time: Time)(block: => T): T = {
+    this.setTime(time)
+    val returnValue = block
+    this.useRealTime()
+
+    returnValue
+  }
+  
   private def useRealTime() {
     this.fake -= 1
   }
@@ -35,14 +58,6 @@ object Echo {
     this.fake += 1
     this.fakeTime = time
   }
-
-  private[echo] def freezeTime[T](time: Time)(block: => T): T = {
-    this.setTime(time)
-    val returnValue = block
-    this.useRealTime()
-
-    returnValue
-  }
 }
 
 private[echo] object Control {
@@ -50,10 +65,19 @@ private[echo] object Control {
   val createLock = new Lock()
   private var devModeOn = false
 
+  /** Can be used to allow FRP code
+    * to execute outside the EchoApp.setup
+    * function (used for tests).
+   */
   def devMode() {
     devModeOn = true
   }
 
+  /** Functions that perform operations
+    * on Behaviours or Events should have their
+    * code wrapped in this block to circumvent
+    * possible semantic problems.
+   */
   def frp[T](block: => T): T = {
     if (createLock.available && !devModeOn) {
       throw new IllegalAccessException("You can't do that...")
