662e2c03cd996c9e36e3cd4c54d2ca184ffb5b17
Callum Stott <oetzi101@gmail.com>
2012-06-15 15:47:11 +0200

    Add scaladoc comments to Cache.class

--- src/main/scala/com/github/oetzi/echo/util/Cache.scala
+++ src/main/scala/com/github/oetzi/echo/util/Cache.scala
@@ -1,11 +1,17 @@
 package com.github.oetzi.echo.util
 
+/** A proxy for non wasteful evaluation of a pure function
+ *  that is evaluated monotonically.
+ */
 class Cache[T, U](work: T => U) {
   private var lastVal: U = null.asInstanceOf[U]
   private var lastCheck: T = null.asInstanceOf[T]
   private var checkedBefore = false
   
   
+  /** Evaluates and returns work with respect to check or returns the cached
+   * value if check is the same as used in the last evaluation.
+   */ 
   def get(check: T): U = {
     if (!checkedBefore || !check.equals(lastCheck)) {
       checkedBefore = true
